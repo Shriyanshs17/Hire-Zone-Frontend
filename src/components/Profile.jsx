f4970e903ff9d2bf9823dc2fd802d7deec72e1c3
Your Name <rishabhs2612@gmail.com>
2024-08-27 18:03:24 +0530

    skills update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -58,7 +58,7 @@ const Profile = () => {
           </div>
         </div>
         <div className="my-5">
-          <h1 className="font-medium my-3">{user?.profile?.skills}</h1>
+          <h1 className="font-medium my-3">Skills</h1>
           <div className="flex items-center gap-1">
             {user?.profile?.skills.length != 0 ? (
               user?.profile?.skills.map((item, index) => <Badge key={index}>{item}</Badge>)
